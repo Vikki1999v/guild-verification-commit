07fd2d6a09c27eec4abc90a4f49c0682f90242d8
W3_Victoria <115560939+Vikki1999v@users.noreply.github.com>
2026-03-06 16:01:44 +0200

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:01:07.378Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T14:01:44.977Z
+Commit: Fix CSS styling bug on mobile devices

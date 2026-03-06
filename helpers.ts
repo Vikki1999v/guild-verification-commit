8b353945bdfd13ac35ef97fb6caf98028d2a2ed4
W3_Victoria <115560939+Vikki1999v@users.noreply.github.com>
2026-03-06 15:57:54 +0200

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T13:57:51.364Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-06T13:57:54.081Z
+Commit: Add multi-language support

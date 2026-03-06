8b595b4e9f6e7208bb4151383fee4fe4eda12133
W3_Victoria <115560939+Vikki1999v@users.noreply.github.com>
2026-03-06 16:02:47 +0200

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:01:44.977Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T14:02:47.412Z
+Commit: Fix memory leak in event handlers

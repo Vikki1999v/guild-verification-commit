57acc634ece04025f88964a3d7f8e4042216cc73
W3_Victoria <115560939+Vikki1999v@users.noreply.github.com>
2026-03-06 16:07:03 +0200

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:05:57.749Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-06T14:07:03.760Z
+Commit: Resolve null pointer exception in user service

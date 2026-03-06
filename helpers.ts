f75874c7b9af31e3df867153e4b6ebb14abf7a06
W3_Victoria <115560939+Vikki1999v@users.noreply.github.com>
2026-03-06 15:57:44 +0200

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T13:57:42.882Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T13:57:44.261Z
+Commit: Resolve null pointer exception in user service

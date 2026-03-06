5cc7ae6f4b319d49e328c5e2ca393f599d7c31cf
W3_Victoria <115560939+Vikki1999v@users.noreply.github.com>
2026-03-06 16:07:05 +0200

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T14:07:03.760Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T14:07:05.911Z
+Commit: Add dark mode support
